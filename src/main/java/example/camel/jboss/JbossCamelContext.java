70a71f5c94dbf2a4cc3c19f5463d5db6ecdceed7
User <roberth@plucky.(none)>
2009-02-26 10:23:33 -0800

    Add @Override annotation

--- src/main/java/example/camel/jboss/JbossCamelContext.java
+++ src/main/java/example/camel/jboss/JbossCamelContext.java
@@ -6,6 +6,7 @@ import org.apache.camel.TypeConverter;
 public class JbossCamelContext
     extends DefaultCamelContext
 {
+    @Override
     protected TypeConverter createTypeConverter() {
         return new JbossTypeConverter(getInjector());
     }
